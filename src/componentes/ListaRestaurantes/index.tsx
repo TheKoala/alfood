612390f3f2d1a34c10e0465d5d82516e502eb720
TheKoala <felipelima8556@gmail.com>
2022-05-11 19:05:16 -0300

    solucao prof

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,31 +1,29 @@
 import { Button, TextField } from '@mui/material';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
 import { IPaginacao } from '../../interfaces/IPaginacao';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 
+interface IParametrosBusca {
+  ordering?: string
+  search?: string
+}
+
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [proximaPagina, setProximaPagina] = useState('');
   const [paginaAnterior, setPaginaAnterior] = useState('');
-  const [busca, setBusca] = useState('');
-
+  const [busca, setBusca] = useState('')
+  
   useEffect(() => {
     carregaDados('http://localhost:8000/api/v1/restaurantes/');
-  },[]);
+  }, []);
 
-  function buscar(evento: React.FormEvent<HTMLFormElement>) {
-    evento.preventDefault()
-    carregaDados('http://localhost:8000/api/v1/restaurantes/')
-  }
-
-  function carregaDados(url: string) {
-    axios.get<IPaginacao<IRestaurante>>(
-      url, 
-      {params: { search: busca}})
+  function carregaDados(url: string, opcoes?: AxiosRequestConfig) {
+    axios.get<IPaginacao<IRestaurante>>(url, opcoes)
       .then(resposta => {
         setRestaurantes(resposta.data.results);
         setProximaPagina(resposta.data.next);
@@ -35,6 +33,18 @@ const ListaRestaurantes = () => {
         console.log(erro);
       })
   }
+  
+  function buscar(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault()
+    const opcoes = {
+      params: { } as IParametrosBusca
+    }
+    if(busca) {
+      opcoes.params.search = busca;
+    }
+    carregaDados('http://localhost:8000/api/v1/restaurantes/', opcoes)
+  }
+
 
   //  'function verMais() {
   //   axios.get<IPaginacao<IRestaurante>>(proximaPagina)
